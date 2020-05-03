47150cf53020b9c2337b76acfbd9a42559c40a0e
unknown <yogesh.wattamwar@sparity.com>
2020-05-03 23:33:20 +0530

    fourth commit

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/webrtc1'));
+app.use(express.static(__dirname + '/dist/webrtc'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/webrtc1/index.html'));
+res.sendFile(path.join(__dirname+'/dist/webrtc/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
